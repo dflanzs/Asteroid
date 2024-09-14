afd3e2cb279e313f407b0e202d5d83c15d48ded0
Daniel lanzas <dlanzaslarra@gmail.com>
2024-09-14 21:20:40 +0200

    Pause menu added

---
+++ Asteroid/Assets/Scripts/PauseScript.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseScript : MonoBehaviour
+{
+    public GameObject PauseButton;
+    public GameObject ResumeButton;
+    public GameObject ResetButton;
+    public GameObject QuitButton;
+    public GameObject PauseMenu;
+    public GameObject pauseImage;
+
+    private bool gamePaused = false;
+
+    void Update(){
+        if(Input.GetKeyDown(KeyCode.Escape)){
+            if (gamePaused){
+                resume();
+            } else {
+                pause();
+            }
+        }
+    }
+
+    public void pause(){
+        Time.timeScale = 0f;
+        gamePaused = true;
+        PauseMenu.SetActive(true); 
+        PauseButton.SetActive(false);
+        ResumeButton.SetActive(true);
+        ResetButton.SetActive(true);
+        QuitButton.SetActive(true);
+        pauseImage.SetActive(true);
+    }
+
+    public void resume(){
+        Time.timeScale = 1f;
+        gamePaused = false;
+        PauseButton.SetActive(true);
+        PauseMenu.SetActive(false); 
+        ResumeButton.SetActive(false);
+        ResetButton.SetActive(false);
+        QuitButton.SetActive(false);
+        pauseImage.SetActive(false);
+    }
+
+    public void restart(){
+        gamePaused = false;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        PauseButton.SetActive(true);
+        PauseMenu.SetActive(false); 
+        ResumeButton.SetActive(false);
+        ResetButton.SetActive(false);
+        QuitButton.SetActive(false);
+        pauseImage.SetActive(false);
+    }
+
+    public void quit(){
+        Debug.Log("Cerrando juego...");
+        Application.Quit();
+    }
+}
